7a901144141d91bf0e304239081e98d1f68dec3e
ssu2026 <yeona2026@gmail.com>
2025-05-29 12:34:51 +0900

    design: main-left 작성 완

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # 삼성화재 클론 코딩
 
+- https://www.samsungfire.com/
+
 ## 반응형 break point
 
 - max-width: 1219px
---
+++ js/swBanner.js
@@ -0,0 +1,10 @@
+const swBanner = new Swiper(".sw-banner", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
